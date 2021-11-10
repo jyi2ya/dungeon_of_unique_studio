d998e1853d6bb3ab5f30de83b8ef0a8141d828d6
jyi2ya <jyi2ya@outlook.com>
2021-11-10 18:18:00 +0800

    完成 ln 命令

--- t2/sh.c
+++ t2/sh.c
@@ -1047,7 +1047,40 @@ int rm_main(int argc, char *argv[])
 
 int ln_main(int argc, char *argv[])
 {
-	puts("ln: not implemented yet");
+	if (argc < 3) {
+		puts("Too few arguments");
+		return 1;
+	}
+	if (argv[2][strlen(argv[2]) - 1] == '/')
+		argv[2][strlen(argv[2]) - 1] = '\0';
+	errno = 0;
+	link(argv[1], argv[2]);
+
+	if (errno == EISDIR) {
+		char *buf = alloc_or_die(NULL, strlen(argv[1]) + strlen(argv[2]) + 2);
+		char *infile;
+		errno = 0;
+
+		for (infile = argv[1]; *infile != '\0'; ++infile)
+			;
+		while (*infile != '/' && infile != argv[1])
+			--infile;
+		if (*infile == '/')
+			++infile;
+
+		strcpy(buf, argv[2]);
+		strcat(buf, "/");
+		strcat(buf, infile);
+
+		link(argv[1], buf);
+		free(buf);
+	}
+
+	if (errno) {
+		perror("ln");
+		return 1;
+	}
+	return 0;
 	return 0;
 }
 
